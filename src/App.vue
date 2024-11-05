cb51f9bf0ddee5ef9e62d0a675f2ad224ddd4697
Vitalii <shut.vitaliy.ol@gmail.com>
2024-11-05 18:44:26 +0200

    connecting to api

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,20 @@ export default {
   methods: {
     async fetchQuote() {
       try {
-        const response = await axios.get('https://favqs.com/api/qotd')
-        this.quote = response.data.quote
-        console.log(this.quote)
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': 'o45M6E68HIpi/yBhHjXbzg==2WN4rgz5KuV0KUzO',
+          },
+        })
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        this.quote = data[0]
+        this.loading = false
       } catch (error) {
         console.error('Fetch error:', error)
         this.loading = false
@@ -23,7 +34,6 @@ export default {
       }
     },
   },
-
   mounted() {
     this.fetchQuote()
   },
@@ -35,7 +45,7 @@ export default {
     <template v-if="!error">
       <div class="quote" v-if="loading">Loading...</div>
       <template v-else>
-        <h1 class="quote">{{ quote }}</h1>
+        <h1 class="quote">{{ quote.quote }}</h1>
         <p class="author">-{{ quote.author }}</p>
       </template>
     </template>
